990c775094820d8b4932539f269793e5adc3fd7f
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-20 17:04:08 +0200

    Session introduced, HttpOnly & Signed cookies discussed

--- examples/cookie-jar/index.js
+++ examples/cookie-jar/index.js
@@ -1,13 +1,20 @@
 import { createServer, startServer } from '#shared';
 import { readFile } from 'fs/promises';
+import crypto from 'crypto';
 
 import db from './database.js';
+import cookieParser from 'cookie-parser';
 
 const app = createServer({ cookies: false });
+const COOKIE_SECRET = 'FLASH -> this must never be stored in code, but in Secrets Manager';
+
+const generateSessionId = () => crypto.randomBytes(16).toString('hex');
+
+app.use(cookieParser(COOKIE_SECRET));
 
 app.get('/', (req, res) => {
   if (!req.cookies) res.send('Cookies are disabled.');
-  if (req.cookies.username) {
+  if (req.signedCookies.username) {
     res.redirect('/profile');
   } else {
     res.redirect('/login');
@@ -17,7 +24,7 @@ app.get('/', (req, res) => {
 app.get('/login', async (req, res) => {
   const loginPage = await readFile('./pages/login.html', 'utf-8');
 
-  if (req.cookies.username) {
+  if (req.signedCookies.username) {
     res.redirect('/profile');
   }
 
@@ -41,7 +48,13 @@ app.post('/login', async (req, res) => {
   );
 
   if (user) {
-    res.cookie('username', username);
+    const sessionId = generateSessionId();
+    await db.run('INSERT INTO sessions (id, username) VALUES (?, ?)', [sessionId, username]);
+    res.cookie('sessionId', sessionId, {
+      httpOnly: true /* FLASH -> makes the cookie inaccessible from JavaScript */,
+      signed: true /* FLASH -> cookie still comes as plain text to the browser, but signed, therefore a modification
+       on the client side will result in a different hash on the server - we know it was tinkered with */
+    });
     res.redirect('/profile');
   } else {
     res.status(403).redirect('/login?error=Invalid login credentials.');
@@ -57,19 +70,19 @@ app.post('/logout', (_, res) => {
 app.get('/profile', async (req, res) => {
   res.locals.title = 'Profile';
 
-  const username = req.cookies.username;
+  const sessionId = req.signedCookies.sessionId;
 
-  if (!username) {
+  if (!sessionId) {
     return res.redirect('/login?error=Please login to view your profile.');
   }
 
-  const user = await db.get('SELECT * FROM users WHERE username = ?', username);
+  const session = await db.get('SELECT * FROM sessions WHERE id = ?', sessionId);
 
-  if (user && user.username) {
+  if (session && session.username) {
     res.send(
       (await readFile('./pages/profile.html', 'utf-8')).replace(
         '{{username}}',
-        user.username
+        session.username
       )
     );
   } else {
